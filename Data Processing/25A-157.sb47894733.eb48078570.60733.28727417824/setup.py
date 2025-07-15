279d6c040438a4fef0601cda37ff020ca96ccc11
Ben Colquhoun <benjamincahoonz@gmail.com>
2025-07-16 11:58:15 +1200

    feat: Add 25A-tab-4733 calib analysis

---
+++ Data Processing/25A-157.sb47894733.eb48078570.60733.28727417824/setup.py
@@ -0,0 +1,5 @@
+msname = "25A-157.sb47894733.eb48078570.60733.28727417824.ms"
+name = '25A-157.sb47894733.eb48078570.60733.28727417824'
+
+execfile("../../cal_tools.py")
+execfile("../../flagging_stats.py")
