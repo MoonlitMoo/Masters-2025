463f411c4313e9425a9731eb18b9c71298e5ba4e
Ben Colquhoun <benjamincahoonz@gmail.com>
2025-07-17 09:22:23 +1200

    chore: Rename folder without spaces

---
+++ Data-Processing/25A-157.sb47896383.eb48084831.60734.58634814815/setup.py
@@ -0,0 +1,9 @@
+msname = "25A-157.sb47896383.eb48084831.60734.58634814815.ms/"
+name = '25A-157.sb47896383.eb48084831.60734.58634814815'
+
+p_calibrator="1331+305=3C286"
+
+execfile("../../cal_tools.py")
+execfile("../../flagging_stats.py")
+
+plotms()
---
+++ Data-Processing/25A-157.sb47897178.eb48143348.60745.94845704861/setup.py
@@ -0,0 +1,10 @@
+msname = "25A-157.sb47897178.eb48143348.60745.94845704861.ms"
+name = '25A-157.sb47897178.eb48143348.60745.94845704861'
+
+p_calibrator="0137+331=3C48"
+s_calibrator="J0424+0204"
+
+execfile("../../cal_tools.py")
+execfile("../../flagging_stats.py")
+
+plotms()
