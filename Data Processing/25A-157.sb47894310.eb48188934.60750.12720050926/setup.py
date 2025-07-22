8a7fad474837fa1ec23e5f193754e72af1f43e57
Ben Colquhoun <benjamincahoonz@gmail.com>
2025-07-22 16:42:02 +1200

    chore: remove deleted files

--- Data Processing/25A-157.sb47894310.eb48188934.60750.12720050926/setup.py
+++
@@ -1,5 +0,0 @@
-msname = "25A-157.sb47894310.eb48188934.60750.12720050926.ms"
-name = '25A-157.sb47894310.eb48188934.60750.12720050926'
-
-execfile("../../cal_tools.py")
-execfile("../../flagging_stats.py")
--- Data Processing/25A-157.sb47895952.eb48256632.60761.33700966435/setup.py
+++
@@ -1,8 +0,0 @@
-msname = "25A-157.sb47895952.eb48256632.60761.33700966435.ms/"
-name = '25A-157.sb47895952.eb48256632.60761.33700966435'
-p_calibrator="1331+305=3C286"
-
-execfile("../../cal_tools.py")
-execfile("../../flagging_stats.py")
-
-plotms()
