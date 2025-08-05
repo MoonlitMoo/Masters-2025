9d47804fbad2c19ddb8b7bc91f99546c2017e614
Ben Colquhoun <benjamincahoonz@gmail.com>
2025-08-06 11:01:06 +1200

    feat: Add 25A-tab-6182 calibration

---
+++ 25A-157.sb47896182.eb48256644.60761.48394760417/casa_pipescript.py
@@ -0,0 +1,26 @@
+# This CASA pipescript is meant for use with CASA 6.6.1 and pipeline 2024.1.0.8
+context = h_init()
+context.set_state('ProjectSummary', 'observatory', 'Karl G. Jansky Very Large Array')
+context.set_state('ProjectSummary', 'telescope', 'EVLA')
+try:
+    hifv_importdata(vis=['25A-157.sb47896182.eb48256644.60761.48394760417.ms'])
+    hifv_flagdata(hm_tbuff='1.5int', fracspw=0.01, intents='*POINTING*,*FOCUS*,*ATMOSPHERE*,*SIDEBAND_RATIO*, *UNKNOWN*, *SYSTEM_CONFIGURATION*, *UNSPECIFIED#UNSPECIFIED*')
+    hifv_vlasetjy()
+    hifv_priorcals(show_tec_maps=False)
+    hifv_aoflagger(flag_target='"1331+305=3C286"', aoflagger_file='light.lua')
+    hifv_testBPdcals()
+    hifv_semiFinalBPdcals()
+    hifv_aoflagger(flag_target='1', aoflagger_file='light.lua', use_corrected=True)
+    hifv_aoflagger(flag_target='3', aoflagger_file='light.lua', use_corrected=True)
+    hifv_solint()
+    hifv_fluxboot()
+    hifv_finalcals()
+    hifv_applycals()
+    hifv_aoflagger(flag_target='2', aoflagger_file='science.lua', use_corrected=True)
+    hifv_aoflagger(flag_target='4', aoflagger_file='science.lua', use_corrected=True)
+    hifv_statwt()
+    hifv_plotsummary()
+    hifv_exportdata()
+finally:
+    h_save()
+
